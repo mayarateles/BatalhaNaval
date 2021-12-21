24b1bb690f5a2cd658ac6de3984b1b5beab5fb02
moisessosa <rod20028@byui.com>
2021-12-21 02:42:29 -0300

    mais arrumado clase jogo e clase tabuleiro

---
+++ src/com/letscode/javabasico/Jogo.java
@@ -0,0 +1,11 @@
+package com.letscode.javabasico;
+
+public class Jogo {
+    public static void main(String[] args){
+        Tabuleiro meuTabuleiro = new Tabuleiro();
+        meuTabuleiro.gerarTabuleiroVazio();
+
+
+       meuTabuleiro.exibirTabuleiro("pedro");
+    }
+}
--- src/com/letscode/javabasico/Tabuleiro.java
+++ src/com/letscode/javabasico/Tabuleiro.java
@@ -1,23 +1,30 @@
 package com.letscode.javabasico;
 
+import java.util.Arrays;
+
 public class Tabuleiro {
-    static char tabuleiro[][] = new char[10][10];
-    static String nomeDoJogador = "Moises";//por enquanto
+    //static String nomeDoJogador = "Moises";//por enquanto
+    public static char[][] tabuleiro= new char[10][10];;
+
     static final char VAZIO = 20;
     static final char NAVIO =78;//ascii
     static final char ERROU_TIRO = 45;//ascii
     static final char ACERTOU_TIRO = 42;//ascii
+    public Tabuleiro() {
 
-    public static void gerarTabuleiroVazio(char[][] tabuleiro){
+
+
+    }
+    public void gerarTabuleiroVazio(){
         for (int i=0; i <10;i++) {
             for (int j = 0; j < 10; j++) {
-                tabuleiro[i][j]=66;
+                tabuleiro[i][j]=20;
             }
 
         }
     }
 
-    public static void exibirTabuleiro(String nomeDoJogador, char[][] tabuleiro){
+    public static void exibirTabuleiro(String nomeDoJogador){
         System.out.println("|------------------ " + nomeDoJogador + " -----------------|");
         System.out.println("---------------------------------------------");
         System.out.println("|   | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |");
@@ -34,8 +41,8 @@ public class Tabuleiro {
                         linhaDoTabuleiro += "  | ";
                         break;
                     case NAVIO :
-                       linhaDoTabuleiro += " N |";
-                       break;
+                        linhaDoTabuleiro += " N |";
+                        break;
 
                     case ERROU_TIRO :
                         linhaDoTabuleiro += " - |";
@@ -48,14 +55,16 @@ public class Tabuleiro {
             }
             System.out.println(linhaDoTabuleiro);
             System.out.println("---------------------------------------------");
-            
+
         }
-        
+
 
     }
-    public static void main(String[] args) {
-    gerarTabuleiroVazio(tabuleiro);
-    exibirTabuleiro(nomeDoJogador,tabuleiro);
 
+    @Override
+    public String toString() {
+        return "Tabuleiro{" +
+                "tabuleiro=" + Arrays.toString(tabuleiro) +
+                '}';
     }
 }
