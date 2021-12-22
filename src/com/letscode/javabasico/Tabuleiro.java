882e03707f8fb2cd21d29945dbfce91ffa41ec79
moisessosa <77083327+moisessosa@users.noreply.github.com>
2021-12-22 08:00:12 -0300

    Update Tabuleiro.java
    tirei os conflitos

--- src/com/letscode/javabasico/Tabuleiro.java
+++ src/com/letscode/javabasico/Tabuleiro.java
@@ -1,70 +1,31 @@
 package com.letscode.javabasico;
 
-import java.util.Arrays;
 
-public class Tabuleiro {
-    //static String nomeDoJogador = "Moises";//por enquanto
-    public static char[][] tabuleiro= new char[10][10];;
+import java.util.Scanner;
 
-    static final char VAZIO = 20;
-    static final char NAVIO =78;//ascii
-    static final char ERROU_TIRO = 45;//ascii
-    static final char ACERTOU_TIRO = 42;//ascii
-    public Tabuleiro() {
+public class Jogo {
+    public static void main(String[] args){
+        Scanner input= new Scanner(System.in);
+        String opcao="X";
+        BemVinda.darBemVinda();//metodo de clase abtracta
+       // Tabuleiro meuTabuleiro = new Tabuleiro();
+        System.out.println("Qual é seu nome?:");
+        String nomeDoJogador = input.next();
+        Jogador jogador = new Jogador(nomeDoJogador);
+        while (!(opcao.equals("1") || opcao.equals("2"))) {
 
-
-
-    }
-    public void gerarTabuleiroVazio(){
-        for (int i=0; i <10;i++) {
-            for (int j = 0; j < 10; j++) {
-                tabuleiro[i][j]=20;
-            }
-
-        }
-    }
-
-    public static void exibirTabuleiro(String nomeDoJogador){
-        System.out.println("|------------------ " + nomeDoJogador + " -----------------|");
-        System.out.println("---------------------------------------------");
-        System.out.println("|   | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |");
-        System.out.println("---------------------------------------------");
-        char letraDaLinha = 65;
-        String linhaDoTabuleiro = "";
-
-        for (char[] linha: tabuleiro) {
-            linhaDoTabuleiro = "| "+(letraDaLinha++) + " | ";
-
-            for (char coluna : linha) {
-                switch(coluna) {
-                    case VAZIO :
-                        linhaDoTabuleiro += "  | ";
-                        break;
-                    case NAVIO :
-                        linhaDoTabuleiro += " N |";
-                        break;
-
-                    case ERROU_TIRO :
-                        linhaDoTabuleiro += " - |";
-                        break;
-
-                    case ACERTOU_TIRO :
-                        linhaDoTabuleiro += " * |";
-                        break;
-                }
-            }
-            System.out.println(linhaDoTabuleiro);
-            System.out.println("---------------------------------------------");
-
-        }
-
-
-    }
-
-    @Override
-    public String toString() {
-        return "Tabuleiro{" +
-                "tabuleiro=" + Arrays.toString(tabuleiro) +
-                '}';
+            System.out.println("Quer posicionar seus navios:");
+            System.out.println("Opção 1-Aleatorio");
+            System.out.println("Opção 2-Manual");
+            opcao = input.next();
+//Temos que faze a logica de que acontece
+        };
+        jogador.tabuleiro.gerarTabuleiroVazio();
+        jogador.tabuleiro.exibirTabuleiro(jogador.getNome());
+        // é uma prova
+        jogador.atirar((char) 1, (char) 3);
+        jogador.atirar((char) 0, (char) 9);
+        jogador.atirar((char) 0, (char) 0);
+        //meuTabuleiro.
     }
 }
