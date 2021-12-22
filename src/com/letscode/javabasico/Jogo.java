26c1efcd5d82b4fb4bf4f350e055cbbf1da20b94
moisessosa <rod20028@byui.com>
2021-12-22 01:15:23 -0300

    tela de Bem vinda

--- src/com/letscode/javabasico/Jogo.java
+++ src/com/letscode/javabasico/Jogo.java
@@ -1,11 +1,25 @@
 package com.letscode.javabasico;
 
+
+import java.util.Scanner;
+
 public class Jogo {
     public static void main(String[] args){
+        Scanner input= new Scanner(System.in);
+        String opcao="X";
+        BemVinda.darBemVinda();//metodo de clase abtracta
         Tabuleiro meuTabuleiro = new Tabuleiro();
+        while (!(opcao.equals("1") || opcao.equals("2"))) {
+
+            System.out.println("Quer posicionar seus navios:");
+            System.out.println("Opção 1-Aleatorio");
+            System.out.println("Opção 2-Manual");
+            opcao = input.next();
+//Temos que faze a logica de que acontece
+        };
         meuTabuleiro.gerarTabuleiroVazio();
 
 
-       meuTabuleiro.exibirTabuleiro("pedro");
+        meuTabuleiro.exibirTabuleiro("pedro");
     }
 }
